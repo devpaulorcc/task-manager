38702f9076ed1ef6478141d16bf5be687d899488
devpaulo2077 <dev.paulorc@gmail.com>
2025-01-26 23:28:34 -0300

    feat: interceptors introdution

--- src/common/interceptors/logger.interceptor.ts
+++ src/common/interceptors/logger.interceptor.ts
@@ -1,8 +1,26 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
-import { Observable } from "rxjs";
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
 
-export class LoggerInterceptor {
-    // intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        
-    // }
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+    const now = Date.now();
+    console.log(`[REQUEST] ${method} ${url} - Inicio da requisição`);
+    return next.handle().pipe(
+      tap(() => {
+        console.log(`[RESPONSE] ${method} ${url} - ${Date.now() - now}ms`);
+      }),
+    );
+  }
 }
